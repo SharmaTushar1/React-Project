ad685d0f83b476e1a45d73ede495fc0821522f9b
SharmaTushar1 <Tusharsharma2440160@gmail.com>
2023-02-21 18:02:49 +0530

    Add files

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import AddTodo from './Components/AddTodo';
+import ListTodo from './Components/ListTodo';
+function App() {
+  return (
+    <div className="App">
+      <AddTodo />
+      <ListTodo />
+    </div>
+  );
+}
+export default App;
